a9126b96447a9e203d5180dfafc3f9a30ad6e70f
Christoph Junghans <junghans@lanl.gov>
2019-02-23 16:25:33 -0700

    libaec: fixed a typo VI

--- review.sh
+++ review.sh
@@ -12,7 +12,7 @@ spectool -g ${PKG}.spec
 rpmbuild -D"_sourcedir ${PWD}" -D"_srcrpmdir ${PWD}" -bs ${PKG}.spec
 
 chown -R review:mock ${H}
-/travis/spinner.sh sudo -u review fedora-review -v --mock-config ${MOCK_CONFIG} -n ${PKG} --mock-options "--no-bootstrap-chroot --no-cleanup-after --no-clean --old-chroot" || EXIT_CODE=1
+sudo -u review fedora-review -v --mock-config ${MOCK_CONFIG} -n ${PKG} --mock-options "--no-bootstrap-chroot --no-cleanup-after --no-clean --old-chroot" || EXIT_CODE=1
 find "review-${PKG}" -name '*.log' -print -exec cat {} ; || true
 cat review-${PKG}/licensecheck.txt || true
 cat review-${PKG}/review.txt || true
