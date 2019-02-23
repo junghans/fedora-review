2bb18de64d795b626c3f1976b19cb2afcbd2e44e
Christoph Junghans <junghans@lanl.gov>
2019-02-23 16:18:33 -0700

    libaec: fixed a typo V

--- review.sh
+++ review.sh
@@ -2,20 +2,18 @@
 
 PKG="$1"
 MOCK_CONFIG="$2"
+EXIT_CODE=0
 H=/travis/$PKG
 
-dnf -q -y install fedora-review spectool
+dnf -q -y install fedora-review spectool sudo
 useradd -d ${H} -g mock review
 cd /travis/$PKG
 spectool -g ${PKG}.spec
 rpmbuild -D"_sourcedir ${PWD}" -D"_srcrpmdir ${PWD}" -bs ${PKG}.spec
 
 chown -R review:mock ${H}
-if ! /travis/spinner.sh su - -c 'fedora-review -v --mock-config ${MOCK_CONFIG} -n ${PKG} --mock-options "--no-bootstrap-chroot --no-cleanup-after --no-clean --old-chroot" review'; then
-  cat .cache/fedora-review.log
-  find  review-${PKG} -name '*.log' -print -exec cat {} ;
-  exit 1
-fi
-find review-${PKG} -name '*.log' -print -exec cat {} ;
-cat review-${PKG}/licensecheck.txt
-cat review-${PKG}/review.txt
+/travis/spinner.sh sudo -u review fedora-review -v --mock-config ${MOCK_CONFIG} -n ${PKG} --mock-options "--no-bootstrap-chroot --no-cleanup-after --no-clean --old-chroot" || EXIT_CODE=1
+find "review-${PKG}" -name '*.log' -print -exec cat {} ; || true
+cat review-${PKG}/licensecheck.txt || true
+cat review-${PKG}/review.txt || true
+exit ${EXIT_CODE}
