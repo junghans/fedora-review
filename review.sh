f7a01fcdc23f413ccf938797b9311700ed89bfea
Christoph Junghans <junghans@lanl.gov>
2019-02-23 17:21:09 -0700

    libaec: fixed a typo VII

--- review.sh
+++ review.sh
@@ -28,8 +28,8 @@ rpmbuild -D"_sourcedir ${PWD}" -D"_srcrpmdir ${PWD}" -bs ${PKG}.spec
 chown -R review:mock ${H}
 set +x
 spin &
-set -x
 SPIN_PID=$!
+set -x
 trap "kill -9 $SPIN_PID" $(seq 0 15)
 if ! sudo -u review fedora-review -v --mock-config ${MOCK_CONFIG} -n ${PKG} --mock-options "--no-bootstrap-chroot --no-cleanup-after --no-clean --old-chroot"; then
   EXIT_CODE=1
